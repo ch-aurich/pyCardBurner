808e0199ca4a6c2911f1c11dfb5f64aeb84b1a97
Christian Aurich <christian.aurich@c-aurich.de>
2014-05-29 15:51:19 +0200

    making pylint even more happy

--- pyCardBurner.py
+++ pyCardBurner.py
@@ -1,23 +1,25 @@
+''' tool to mass flash SD (or other) mass storage cards '''
 import dbus
 #import gobject
 from dbus.mainloop.glib import DBusGMainLoop
 from PySide import QtGui
 import sys
-import time
-
 
 import BurnerProgressWidget
 
-DBusGMainLoop(set_as_default=True) #Inform that a main loop will be used
+DBusGMainLoop(set_as_default=True)  #Inform that a main loop will be used
 
-class waitWindow(QtGui.QWidget):
+
+class WaitWindow(QtGui.QWidget):
     def __init__(self):
-        super(waitWindow, self).__init__()
+        super(WaitWindow, self).__init__()
+        self.list = QtGui.QListWidget()
         self.initUI()
+
     def addDevice(self, name):
-        newItem = QtGui.QListWidgetItem()
-        newItem.setText(name)
-        self.list.insertItem(0, newItem)
+        new_item = QtGui.QListWidgetItem()
+        new_item.setText(name)
+        self.list.insertItem(0, new_item)
 
     def initUI(self):
         description = QtGui.QLabel("Plug in your SD cards \
@@ -25,36 +27,39 @@ class waitWindow(QtGui.QWidget):
                       window when you plugged in all cards. \
                       Detected cards will be listed below:")
         self.setWindowTitle('waiting...')
-        self.list = QtGui.QListWidget()
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(description)
         vbox.addWidget(self.list)
         self.setLayout(vbox)
 
-class pyCardBurner(QtGui.QWidget):
-    def __init__(self, inputfile, dev_list):
+
+class CardBurner(QtGui.QWidget):
+    def __init__(self, imagefile, devices):
         ''' initializes the pyCardBurner object
-        number is the number of burner slots '''
-        super(pyCardBurner, self).__init__()
-        self.dev_list = dev_list
-        self.inputfile = inputfile
+        dev_list is the dictionary of device names to be used '''
+        super(CardBurner, self).__init__()
+        self.dev_list = devices
+        self.inputfile = imagefile
         self.initUI()
 
     def closeEvent(self, event):
+        ''' event for window close request (i.e. clicking on the X) '''
         if self.none_busy():
-            print "stopping all threads for exit"
+            print("stopping all threads for exit")
             self.stop_all()
             event.accept()
         else:
-            print "ignoring users wish to close window, \
-                   some card is not flashed yet"
+            print("ignoring users wish to close window, \
+                   some card is not flashed yet")
             event.ignore()
 
     def stop_all(self):
+        ''' method to tell all burner widgets to  stop their work '''
         for devicename, burner in self.dev_list.items():
             burner['burner'].stop()
 
     def none_busy(self):
+        ''' find out if all burners are idle '''
         none_busy = True
         for devicename, burner in self.dev_list.items():
             if burner['burner'].is_busy() == True:
@@ -62,6 +67,8 @@ class pyCardBurner(QtGui.QWidget):
         return none_busy
 
     def udisks_device_changed(self, devPath, deviceName, deviceSize):
+        ''' event (used as callback from udisks) to signalize
+        the change of device properties '''
         if deviceSize == 0:
             if not self.dev_list[deviceName]['size'] == 0:
                 self.dev_list[deviceName]['burner'].drive_removed()
@@ -72,15 +79,16 @@ class pyCardBurner(QtGui.QWidget):
                 self.dev_list[deviceName]['size'] = deviceSize
 
     def initUI(self):
+        ''' initialize the user interface '''
         self.burners = []
         self.setWindowTitle('pyCardBurner')
 
         hbox = QtGui.QHBoxLayout()
         index = 0
         for deviceName, deviceproperties in self.dev_list.items():
-            self.burners.\
-                append(BurnerProgressWidget.BurnerProgressWidget\
-                (deviceName, self.inputfile))
+            self.burners. \
+                append(BurnerProgressWidget.BurnerProgressWidget \
+                           (deviceName, self.inputfile))
             hbox.addWidget(self.burners[index])
             self.dev_list[deviceName]['burner'] = self.burners[index]
             self.dev_list[deviceName]['burnerIndex'] = index
@@ -93,20 +101,21 @@ def on_device_changed(dev_path):
     added_dev_obj = bus.get_object("org.freedesktop.UDisks", dev_path)
     added_dev = dbus.Interface(added_dev_obj, 'org.freedesktop.UDisks')
     added_dev_props = dbus.Interface(added_dev_obj, dbus.PROPERTIES_IFACE)
-    deviceFile = str(added_dev_props.\
-                 Get('org.freedesktop.UDisks.Device', "DeviceFile"))
-    deviceSize = int(added_dev_props.\
-                 Get('org.freedesktop.UDisks.Device', "DeviceSize"))
-    deviceIsPartition = bool(added_dev_props.\
-                 Get('org.freedesktop.UDisks.Device', "DeviceIsPartition"))
+    deviceFile = str(added_dev_props. \
+                     Get('org.freedesktop.UDisks.Device', "DeviceFile"))
+    deviceSize = int(added_dev_props. \
+                     Get('org.freedesktop.UDisks.Device', "DeviceSize"))
+    deviceIsPartition = bool(added_dev_props. \
+                             Get('org.freedesktop.UDisks.Device', \
+                                 "DeviceIsPartition"))
 
     if learningmode == True:
         if deviceFile in dev_list \
-          and not deviceIsPartition \
-          and int(deviceSize) > 0 \
-          and dev_list[deviceFile]['size'] == 0:
+                and not deviceIsPartition \
+                and int(deviceSize) > 0 \
+                and dev_list[deviceFile]['size'] == 0:
             dev_list[deviceFile]['size'] = deviceSize
-            print "added " + deviceFile + " to the list of used devices"
+            print("added " + deviceFile + " to the list of used devices")
             waitwindow.addDevice(deviceFile)
     else:
         if deviceFile in dev_list and not deviceIsPartition:
@@ -117,28 +126,29 @@ raw_input("Connect all your card readers without SD cards inserted and \
            press Enter to start the learning phase")
 learningmode = True
 bus = dbus.SystemBus()
-ud_manager_obj = bus.get_object("org.freedesktop.UDisks", \
-                                "/org/freedesktop/UDisks")
+ud_manager_obj = bus.get_object('org.freedesktop.UDisks', \
+                                '/org/freedesktop/UDisks')
 ud_manager = dbus.Interface(ud_manager_obj, 'org.freedesktop.UDisks')
 ud_manager.connect_to_signal('DeviceChanged', on_device_changed)
 
 dev_list = {}
 
 for dev in ud_manager.EnumerateDevices():
-    device_obj = bus.get_object("org.freedesktop.UDisks", dev)
+    device_obj = bus.get_object('org.freedesktop.UDisks', dev)
     device_props = dbus.Interface(device_obj, dbus.PROPERTIES_IFACE)
-    deviceFile = str(device_props.\
-                 Get('org.freedesktop.UDisks.Device', "DeviceFile"))
-    deviceSize = int(device_props.\
-                 Get('org.freedesktop.UDisks.Device', "DeviceSize"))
-    deviceIsPartition = bool(device_props.\
-                 Get('org.freedesktop.UDisks.Device', "DeviceIsPartition"))
+    deviceFile = str(device_props. \
+                     Get('org.freedesktop.UDisks.Device', 'DeviceFile'))
+    deviceSize = int(device_props. \
+                     Get('org.freedesktop.UDisks.Device', 'DeviceSize'))
+    deviceIsPartition = bool(device_props. \
+                             Get('org.freedesktop.UDisks.Device', \
+                                 'DeviceIsPartition'))
 
     if not deviceIsPartition and deviceSize == 0:
         dev_list[deviceFile] = {'size': 0}
 
 app = QtGui.QApplication(sys.argv)
-waitwindow = waitWindow()
+waitwindow = WaitWindow()
 waitwindow.show()
 app.exec_()
 
@@ -150,6 +160,6 @@ for dev, val in dev_list.items():
 
 inputfile = str(sys.argv[1])
 
-pdb = pyCardBurner(inputfile, dev_list)
+pdb = CardBurner(inputfile, dev_list)
 pdb.show()
 sys.exit(app.exec_())
